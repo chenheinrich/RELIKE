53b08968107679b9d3fba02c166da779f02cc459
Chen Heinrich <chenhe@caltech.edu>
2021-02-09 17:10:30 -0800

    made example_likelihood.py into executable format with main()

--- examples/example_likelihood.py
+++ examples/example_likelihood.py
@@ -7,53 +7,67 @@ import numpy as np
 
 import erlike as erl
 
-pc = erl.PC()
-gauss_like = erl.GaussianLikelihood()
-tanh_model = erl.TanhModel()
 
-# Timing
-#@profiler
-#def get_mjs(func):
-#    [pc.get_mjs(func) for i in range(1000)]
-#timing_test = pc.get_mjs(xe_func_1)
+def example_1():
 
-# Plot PC and fiducial xe(z)
-#pc.data.plot_pc()
+    pc = erl.PC()
+    gauss_like = erl.GaussianLikelihood()
+    tanh_model = erl.TanhModel()
 
-# TODO to polish
-xe_func = tanh_model.get_xe_func(zre=8.27789306640625, no_helium=True)
-xe_func_bf = tanh_model.get_xe_func(zre=7.1, no_helium=True)
-mjs = pc.get_mjs(xe_func)
+    #TODO put in other example scripts?
+    #TODO put in jupyter notebook?
+    # Timing
+    #@profiler
+    #def get_mjs(func):
+    #    [pc.get_mjs(func) for i in range(1000)]
+    #timing_test = pc.get_mjs(xe_func_1)
 
-mjs_bf = pc.get_mjs(xe_func_bf)
+    # Plot PC and fiducial xe(z)
+    #pc.data.plot_pc()
 
-loglike = gauss_like.get_loglike(mjs)
-loglike_bf = gauss_like.get_loglike(mjs_bf)
+    xe_func = tanh_model.get_xe_func(zre=8.27789306640625, no_helium=True)
+    xe_func_bf = tanh_model.get_xe_func(zre=7.1, no_helium=True)
+    mjs = pc.get_mjs(xe_func)
 
-print('mjs = {}\n'.format(mjs))
-print('loglike = {}\n'.format(loglike))
+    mjs_bf = pc.get_mjs(xe_func_bf)
 
-pc.data.plot_xe(mjs, xe_func=xe_func)
+    loglike = gauss_like.get_loglike(mjs)
+    loglike_bf = gauss_like.get_loglike(mjs_bf)
 
-likelihood_ratio = np.exp(loglike-loglike_bf)
-print('likelihood ratio to best-fit Planck 2018 tanh model is: \
-    {}'.format(likelihood_ratio))
+    print('mjs = {}\n'.format(mjs))
+    print('loglike = {}\n'.format(loglike))
 
-chi2 = 2.0 * (loglike-loglike_bf)
-print('chi2 between this model and the best-fit Planck 2018 tanh model is: \
-    {}\n'.format(chi2))
+    pc.data.plot_xe(mjs, xe_func=xe_func)
 
-use_fiducial_cosmology = True
-tau = pc.get_tau(mjs, use_fiducial_cosmology) #TODO to be polished
-tau_real = .059997
+    likelihood_ratio = np.exp(loglike-loglike_bf)
+    print('likelihood ratio to best-fit Planck 2018 tanh model is: \
+        {}'.format(likelihood_ratio))
 
-print('PC estimated tau = {}'.format(tau)) 
-print('real tau = {}\n'.format(tau_real))
+    chi2 = 2.0 * (loglike-loglike_bf)
+    print('chi2 between this model and the best-fit Planck 2018 tanh model is: \
+        {}\n'.format(chi2))
 
-# Example 2:
+    use_fiducial_cosmology = True
+    tau = pc.get_tau(mjs, use_fiducial_cosmology) #TODO to be polished
+    tau_real = .059997
 
-# Plot out tanh posterior (evaluated w/ Gaussian likelihood at points)
+    print('PC estimated tau = {}'.format(tau)) 
+    print('real tau = {}\n'.format(tau_real))
 
-# specify zre, make xe(z); get tau value by integrating (might wanna work on integration to be fast)
+def main():
+    example_1()
+    example_2()
 
-# plot Gaussian likelihood at these points for the posterior
+def example_2():
+    pass
+    #TODO implement
+    # Example 2:
+
+    # Plot out tanh posterior (evaluated w/ Gaussian likelihood at points)
+
+    # specify zre, make xe(z); get tau value by integrating (might wanna work on integration to be fast)
+
+    # plot Gaussian likelihood at these points for the posterior
+
+if __name__ == '__main__':
+    main()

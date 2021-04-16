33923242e268b10c92e1d606a455add56b0b85b4
Chen Heinrich <chenhe@caltech.edu>
2021-04-15 17:36:52 -0700

    updated requirements.txt and delete obsolete data

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name="relike", 
+setup(
+    name="relike", 
+    version='0.0.1',
+    author='Chen Heinrich',
+    author_email='chenhe@caltech.edu',
+    description=('Python package for the Reionization Effective Likelihood (RELIKE) from Planck 2018 data'),
+    url='https://github.com/chenheinrich/RELIKE',
+    license='BSD',
+
     packages=find_packages(),
     install_requires=['scipy>=1.6.0', 'numpy>=1.18.5', 
         'pytest>=5.4.3', 'profiler>=0.1.0'],
-    python_requires='>=3.6')
+    python_requires='>=3.6',
+    include_package_data=True,
+    package_data={'': ['data/*.dat']},
+    )

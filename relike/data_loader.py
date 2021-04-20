f8c0821afbc0950099c4dd31940c02c6e39674a4
Chen Heinrich <chenhe@caltech.edu>
2021-04-20 13:45:18 -0700

    pip install . now works

--- relike/data_loader.py
+++ relike/data_loader.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import yaml
+import pkg_resources
 
 class DataLoader():
 
--- setup.py
+++ setup.py
@@ -10,9 +10,14 @@ setup(
     license='BSD',
 
     packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
     install_requires=['scipy>=1.6.0', 'numpy>=1.18.5', 
         'pytest>=5.4.3', 'profiler>=0.1.0'],
     python_requires='>=3.6',
     include_package_data=True,
-    package_data={'': ['data/*.dat']},
+    package_data={"": ["data/*.dat", "data/*.yaml"]},
     )
